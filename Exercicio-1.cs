28ec4fcb8286f461c43cb87a42211d3c3f65c079
Isabela Bergamo <105016708+itBergamo@users.noreply.github.com>
2025-01-09 22:11:21 -0300

    Respondendo a resolução inteira

--- Exercicio-1.cs
+++ Exercicio-1.cs
@@ -1,10 +1,63 @@
-//1. Observe o trecho de código abaixo: 
-// int INDICE = 13
-// SOMA = 0
-// K = 0
+//  1. Observe o trecho de código abaixo: 
+//   int INDICE = 13
+//   SOMA = 0
+//   K = 0
 //
-// Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
-// Imprimir(SOMA);
+//   Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+//   Imprimir(SOMA);
 //
 // Ao final do processamento, qual será o valor da variável SOMA?
-//R: 91
+// 
+//  0 < 13 
+//  Então K = 0 + 1; SOMA = 0 + 1
+//  SOMA = 1
+// 
+//  1 < 13
+//  Então K = 1 + 1; SOMA = 1 + 2
+//  SOMA = 3
+// 
+//  2 < 13
+//  Então K = 2 + 1; SOMA = 3 + 3
+//  SOMA = 6
+// 
+//  3 < 13
+//  Então K = 3 + 1; SOMA = 6 + 4
+//  SOMA = 10
+// 
+//  4 < 13
+//  Então K = 4 + 1; SOMA = 10 + 5
+//  SOMA = 15
+// 
+//  5 < 13
+//  Então K = 5 + 1; SOMA = 15 + 6
+//  SOMA = 21
+// 
+//  6 < 13
+//  Então K = 6 + 1; SOMA = 21 + 7
+//  SOMA = 28
+// 
+//  7 < 13
+//  Então K = 7 + 1; SOMA = 28 + 8
+//  SOMA = 36
+// 
+//  8 < 13
+//  Então K = 8 + 1; SOMA = 36 + 9
+//  SOMA = 45
+// 
+//  9 < 13
+//  Então K = 9 + 1; SOMA = 45 + 10
+//  SOMA = 55
+// 
+//  10 < 13
+//  Então K = 10 + 1; SOMA = 55 + 11
+//  SOMA = 66
+// 
+//  11 < 13
+//  Então K = 11 + 1; SOMA = 66 + 12
+//  SOMA = 78
+//
+//  12 < 13
+//  Então K = 12 + 1; SOMA = 78 + 13
+//  SOMA = 91
+//
+//  R: 91
